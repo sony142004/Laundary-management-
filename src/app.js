214a3491c1720d0a7b93ca1f523efbcd27d26424
SAI SOWMYA <sowmyachandra54@gmail.com>
2026-04-17 11:46:31 +0530

    Chore: Update server config for cloud deployment

--- src/app.js
+++ src/app.js
@@ -20,7 +20,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    console.log(`Try POST http://localhost:${PORT}/orders to create an order`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
